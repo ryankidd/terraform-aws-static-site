ee9d47bde7e704b3d3a02b65fe9a67807443fc11
Ryan Kidd <itsryankidd@gmail.com>
2026-08-06 11:23:25 -0700

    Add S3 bucket for site content

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "site" {
+  bucket = var.bucket_name
+  tags   = var.tags
+}
+
+resource "aws_s3_bucket_public_access_block" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
