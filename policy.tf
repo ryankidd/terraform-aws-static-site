38bd569427b27609a4aafd524b2e0b5c9f8dadd3
Ryan Kidd <itsryankidd@gmail.com>
2026-08-07 08:39:36 -0700

    Grant CloudFront OAC read access to the S3 bucket

---
+++ policy.tf
@@ -0,0 +1,23 @@
+data "aws_iam_policy_document" "site" {
+  statement {
+    sid       = "AllowCloudFrontServicePrincipal"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.site.arn}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.site.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "site" {
+  bucket = aws_s3_bucket.site.id
+  policy = data.aws_iam_policy_document.site.json
+}
