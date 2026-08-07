0e71a611fe693bcc4ac1dfae0401797d320df8ac
Ryan Kidd <itsryankidd@gmail.com>
2026-08-07 08:39:41 -0700

    Add CloudFront distribution outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "bucket_arn" {
   description = "ARN of the S3 bucket storing site content."
   value       = aws_s3_bucket.site.arn
 }
+
+output "distribution_id" {
+  description = "ID of the CloudFront distribution."
+  value       = aws_cloudfront_distribution.site.id
+}
+
+output "distribution_domain_name" {
+  description = "Domain name of the CloudFront distribution."
+  value       = aws_cloudfront_distribution.site.domain_name
+}
