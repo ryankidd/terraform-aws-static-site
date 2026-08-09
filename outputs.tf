3909a30a6d5bef6096706d682a9e25253be09b5e
Ryan Kidd <itsryankidd@gmail.com>
2026-08-09 10:35:54 -0700

    Add output for the access log bucket

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,8 @@ output "distribution_domain_name" {
   description = "Domain name of the CloudFront distribution."
   value       = aws_cloudfront_distribution.site.domain_name
 }
+
+output "logs_bucket_id" {
+  description = "ID of the S3 bucket storing CloudFront access logs, if enable_logging is true."
+  value       = try(aws_s3_bucket.logs[0].id, null)
+}
